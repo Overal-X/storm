18833053788fc3137ab4d9ff8e480d84ffd03c04
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2024-08-19 07:31:14 +0100

    build(scripts): updates script to `v0.0.8`

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Default version
-DEFAULT_VERSION="v0.0.7"
+DEFAULT_VERSION="v0.0.8"
 
 # Get the version from the command line argument or use default
 VERSION=${1:-$DEFAULT_VERSION}
