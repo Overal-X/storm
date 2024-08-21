a84091845cd1993c13c46806d1b60dd503ffc73f
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2024-08-21 12:01:04 +0100

    build(scripts): upgrades scripts to `v0.0.12`

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,5 +1,5 @@
 # Default version
-$DefaultVersion = "v0.0.11"
+$DefaultVersion = "v0.0.12"
 
 # Get the version from the command line argument or use default
 $Version = $args[0]
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Default version
-DEFAULT_VERSION="v0.0.11"
+DEFAULT_VERSION="v0.0.12"
 
 # Get the version from the command line argument or use default
 VERSION=${1:-$DEFAULT_VERSION}
