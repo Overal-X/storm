dc9c277c454826c86fff4119a71121808750795c
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2024-08-27 15:42:52 +0100

    build(scripts): updates script default installation version to `v0.0.15`

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Default version
-DEFAULT_VERSION="v0.0.14"
+DEFAULT_VERSION="v0.0.15"
 
 # Get the version from the command line argument or use default
 VERSION=${1:-$DEFAULT_VERSION}
