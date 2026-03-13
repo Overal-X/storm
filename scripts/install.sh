6719e3323919fb6f219d80b4616b681200040f89
Mohammed Al-Ameen <ameenmohammed2311@gmail.com>
2026-03-12 23:46:13 -0700

    Update default version in install script to v0.0.18

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Default version
-DEFAULT_VERSION="v0.0.16"
+DEFAULT_VERSION="v0.0.18"
 
 # Get the version from the command line argument or use default
 VERSION=${1:-$DEFAULT_VERSION}
